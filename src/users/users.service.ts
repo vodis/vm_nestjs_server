832b30687009f768f71e2b085527b7ceb8967660
voytenko.d <voytenko.d@ideasoft.io>
2019-12-18 18:17:00 +0200

    Users passwords are hashed before storing

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
 
+import * as bcrypt from 'bcryptjs';
 import * as uuidv4 from 'uuid/v4';
 import * as jwt from 'jsonwebtoken';
 import tokenConfig from '../config/token.config';
@@ -37,7 +38,6 @@ export class UsersService {
       );
     }
 
-    const newUser = user;
     const _id = uuidv4();
     user.id = _id;
     user.token = jwt.sign(
@@ -45,9 +45,11 @@ export class UsersService {
       tokenConfig.secretKey,
       tokenConfig.expiryDate,
     );
+    user.password = await bcrypt.hash(user.password, 8);
     user.create_at = new Date();
     user.update_at = new Date();
-    return await this.usersRepository.save(newUser);
+
+    return await this.usersRepository.save(user);
   }
 
   async updateUser(user: User) {
